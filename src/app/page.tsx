6fe24a5dfb7f2cba898c182eb36cced896674440
mamangesjeruksiboy <adryanryan354@gmail.com>
2026-06-08 10:39:26 +0700

    fix base url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -177,7 +177,7 @@ export default function Home() {
               wordBreak: "break-all",
             }}
           >
-            {process.env.NEXT_PUBLIC_BASE_URL ?? "https://doa-harian.vercel.app"}
+            {process.env.NEXT_PUBLIC_BASE_URL ?? "https://doa-harian-opal.vercel.app"}
           </div>
         </section>
 
